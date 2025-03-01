85649e80f01511ce0b933b1253900f85dfb97d7f
CrisParp <parpcrismyself@gmail.com>
2025-02-28 19:21:38 -0700

    ,,,

--- script.js
+++ script.js
@@ -1,79 +1,15 @@
-window.onload = function() {
-    const canvas = document.getElementById('wallpaperCanvas');
-    const ctx = canvas.getContext('2d');
+function randomPosition() {
+    const x = Math.floor(Math.random() * window.innerWidth);
+    const y = Math.floor(Math.random() * window.innerHeight);
+    return { x, y };
+}
 
-    if (!ctx) {
-        console.error('Canvas context not initialized');
-        return;
-    }
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    let particlesArray = [];
-    const numberOfParticles = 100;
-
-    class Particle {
-        constructor(x, y, directionX, directionY, size, color) {
-            this.x = x;
-            this.y = y;
-            this.directionX = directionX;
-            this.directionY = directionY;
-            this.size = size;
-            this.color = color;
-        }
-
-        draw() {
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-            ctx.fillStyle = this.color;
-            ctx.fill();
-        }
-
-        update() {
-            if (this.x + this.size > canvas.width || this.x - this.size < 0) {
-                this.directionX = -this.directionX;
-            }
-            if (this.y + this.size > canvas.height || this.y - this.size < 0) {
-                this.directionY = -this.directionY;
-            }
-
-            this.x += this.directionX;
-            this.y += this.directionY;
-
-            this.draw();
-        }
-    }
-
-    function init() {
-        particlesArray = [];
-        for (let i = 0; i < numberOfParticles; i++) {
-            let size = Math.random() * 5 + 1;
-            let x = Math.random() * (canvas.width - size * 2) + size;
-            let y = Math.random() * (canvas.height - size * 2) + size;
-            let directionX = (Math.random() * 2) - 1;
-            let directionY = (Math.random() * 2) - 1;
-            let color = 'rgba(255, 255, 255, 0.8)';
-
-            particlesArray.push(new Particle(x, y, directionX, directionY, size, color));
-        }
-    }
-
-    function animate() {
-        requestAnimationFrame(animate);
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        for (let i = 0; i < particlesArray.length; i++) {
-            particlesArray[i].update();
-        }
-    }
-
-    init();
-    animate();
-
-    window.addEventListener('resize', function() {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        init();
-    });
-};
+document.querySelectorAll('.circle').forEach(circle => {
+    const size = Math.random() * 100 + 50;
+    circle.style.width = `${size}px`;
+    circle.style.height = `${size}px`;
+    const { x, y } = randomPosition();
+    circle.style.left = `${x}px`;
+    circle.style.top = `${y}px`;
+    circle.style.animationDuration = `${Math.random() * 20 + 10}s`;
+});
